cdbf1da11d54835f3203475b4bcda9272ad6b12b
bens <bens@spreadshirt.net>
2020-02-07 18:35:57 +0100

    proper header .. "artwork"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { classes, stylesheet } from "typestyle";
 import Calendar from "./Calendar";
+import headerImage from "./assets/laura-siegal-1xmuSigXQ2Y-unsplash.jpg";
 
 const styles = stylesheet({
   fullHeight: { height: "100vh" },
@@ -16,7 +17,19 @@ const styles = stylesheet({
     gridColumnEnd: 4,
     gridRowStart: 1,
     gridRowEnd: 2,
-    backgroundColor: "lightcyan"
+    backgroundImage: `url(${headerImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    borderBottom: "1px solid rgba(206, 175, 142, 0.41)"
+  },
+  description: {
+    margin: "90px",
+    color: "white",
+    maxWidth: "300px",
+    filter: "drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.28))"
+  },
+  descriptionDate: {
+    marginTop: "1em"
   },
   footer: {
     gridColumnStart: 1,
@@ -43,7 +56,18 @@ const App: React.FC = () => {
 };
 
 const Artwork: React.FC = () => {
-  return <div className={styles.artwork}>Artwork</div>;
+  return (
+    <div className={styles.artwork}>
+      <div className={styles.description}>
+        <h1>Ostern</h1>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor
+          quis velit sit amet iaculis. Nulla volutpat.
+        </div>
+        <div className={styles.descriptionDate}>07.04.2020 – 11.04.2020</div>
+      </div>
+    </div>
+  );
 };
 
 const Footer: React.FC = () => {
