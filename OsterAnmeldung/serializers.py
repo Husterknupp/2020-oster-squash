4514d57979d6a1e40ccbff1689a29c43f29392a4
bens <bens@spreadshirt.net>
2020-03-02 22:31:26 +0100

    hide delicate email field for GET endpoints

--- OsterAnmeldung/serializers.py
+++ OsterAnmeldung/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+
 from .models import Registration
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
-        fields = ('id', 'emailAddress', 'timeFrameBegin', 'dateOfRegistration', 'adultsCount', 'childCount', 'state')
+        # fields for /api/registrations/
+        # def to_representation(self, value) is the method that might come in handy later
+        fields = ('id', 'timeFrameBegin', 'adultsCount', 'childCount', 'state')
