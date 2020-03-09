ea07c73ca586834bcff535c7d0e7ddc7d1f9e70a
bens <bens@spreadshirt.net>
2020-03-10 00:24:03 +0100

    refactor Imprint/Impressum to be less noisy

--- frontend/src/Impressum.tsx
+++ frontend/src/Impressum.tsx
@@ -1,111 +1,90 @@
 import React, { ReactElement } from 'react';
 import { stylesheet } from 'typestyle';
-import { H1, SUBTLE_LINK } from './constants';
+import { SUBTLE_LINK } from './constants';
 
 const styles = stylesheet({
     centralGridBlock: {
         gridColumnStart: 2,
         gridColumnEnd: 3,
     },
-    header: {
-        ...H1,
-        margin: '16px',
-    },
-    content: {
-        margin: '0 16px',
-        $nest: {
-            '& > div': {
-                margin: '.5em 0',
-            },
-        },
-    },
     bold: {
         fontWeight: 'bold',
-    },
-    address: {
-        margin: '.3em 0',
     },
 });
 
 export function Impressum(): ReactElement {
     return (
         <div className={styles.centralGridBlock}>
-            <div className={styles.header}>Impressum</div>
-            <div className={styles.content}>
+            <h2>Impressum</h2>
+            Wer ist wofür verantwortlich?
+            <h3 className={styles.bold}>Ostern ERlebt</h3>
+            <p>
+                Vor 2000 Jahren hat Jesus Christus durch seinen Tod und seine Auferstehung die
+                Weltgeschichte nachhaltig verändert. Doch wie war das eigentlich damals? Eine Reise
+                in die Vergangenheit leitet Sie nach Jerusalem und lässt Sie Ostern mit allen Sinnen
+                erfassen.
+                <br />
+                Eine professionelle Reisebegleitung führt Sie Schritt für Schritt durch die
+                biblischen Ereignisse. Dieser Weg ermöglicht eine lebendige Erfahrung, die bis heute
+                Menschenleben berührt und verändert.
+            </p>
+            <p>
+                <div>Sind Sie neugierig geworden?</div>
+                <div>Haben Sie Mut, sich auf etwas Außergewöhnliches einzulassen!</div>
+            </p>
+            <p>
                 <div>
-                    <span className={styles.bold}>Ostern ERlebt</span>
-                    <div>
-                        Vor 2000 Jahren hat Jesus Christus durch seinen Tod und seine Auferstehung
-                        die Weltgeschichte nachhaltig verändert. Doch wie war das eigentlich damals?
-                        Eine Reise in die Vergangenheit leitet Sie nach Jerusalem und lässt Sie
-                        Ostern mit allen Sinnen erfassen.
-                        <br />
-                        Eine professionelle Reisebegleitung führt Sie Schritt für Schritt durch die
-                        biblischen Ereignisse. Dieser Weg ermöglicht eine lebendige Erfahrung, die
-                        bis heute Menschenleben berührt und verändert.
-                    </div>
-                    <div>
-                        Sind Sie neugierig geworden?
-                        <br />
-                        Haben Sie Mut, sich auf etwas Außergewöhnliches einzulassen!
-                    </div>
-                    <div>
-                        Bitte planen Sie mindestens eine Stunde für die Rundreise ein. Anschließend
-                        gibt es noch offene Angebote zum Verweilen.
-                        <br />
-                        Sie können sich als Einzelperson oder auch als Gruppe (max. 8 Personen)
-                        anmelden.
-                    </div>
-                    <div>
-                        Hinweis: Um Ihre Zeit in Jerusalem so lückenlos und stressfrei wie möglich
-                        erleben zu können, möchten wir alle Teilnehmenden bitten, pünktlich zu sein
-                    </div>
+                    Bitte planen Sie mindestens eine Stunde für die Rundreise ein. Anschließend gibt
+                    es noch offene Angebote zum Verweilen.
                 </div>
                 <div>
-                    Die St. Georgen-Kirche gehört zur{' '}
-                    <span className={styles.bold}>Evangeliumsgemeinde Halle e. V. </span>(
-                    <a href={'https://evangeliumsgemeinde.de'}>evangeliumsgemeinde.de</a>):
-                    <div>Glauchaer Str. 77</div>
-                    <div>06110 Halle/S.</div>
+                    Sie können sich als Einzelperson oder auch als Gruppe (max. 8 Personen)
+                    anmelden.
                 </div>
-                <div>
-                    Veranstalter ist der{' '}
-                    <span className={styles.bold}>Gemeinsam für Halle e. V. </span>(
-                    <a href={'https://gfhalle.de'}>gfHalle.de</a>)
-                </div>
-                <div>
-                    <div className={styles.bold}>Internetauftritt: </div>
-                    <div>
-                        Anbieter und verantwortlich für den Inhalt dieser Seite ist
-                        <div className={styles.address}>
-                            Antonia Gerdemann
-                            <br />
-                            Wörmlitzer Str. 101
-                            <br />
-                            06110 Halle/S.
-                        </div>
-                        Entwickelt hat sie Benjamin Schandera aus Halle (husterknupp at outlook.com)
-                        mit freier Software, gehostet auf{' '}
-                        <a
-                            href={'https://github.com/Husterknupp/2020-oster-squash'}
-                            target={'_blank'}
-                        >
-                            GitHub.com
-                        </a>{' '}
-                        und{' '}
-                        <a href={'https://uberspace.de'} target={'_blank'}>
-                            Uberspace.de
-                        </a>
-                    </div>
-                </div>
-                <div>
-                    <span className={styles.bold}>Bildnachweis: </span> Bruno von der Kraan (
-                    <a href={'https://unsplash.com/photos/v2HgNzRDfII'} target={'_blank'}>
-                        Unsplash.com
-                    </a>
-                    )
-                </div>
-            </div>
+            </p>
+            <p>
+                Hinweis: Um Ihre Zeit in Jerusalem so lückenlos und stressfrei wie möglich erleben
+                zu können, möchten wir alle Teilnehmenden bitten, pünktlich zu sein.
+            </p>
+            <h3>Ort & Veranstalter</h3>
+            <p>
+                Die St. Georgen-Kirche gehört zur{' '}
+                <span className={styles.bold}>Evangeliumsgemeinde Halle e. V. </span>(
+                <a href={'https://evangeliumsgemeinde.de'}>evangeliumsgemeinde.de</a>):
+                <div>Glauchaer Str. 77</div>
+                <div>06110 Halle/S.</div>
+            </p>
+            <p>
+                Veranstalter ist der <span className={styles.bold}>Gemeinsam für Halle e. V. </span>
+                (<a href={'https://gfhalle.de'}>gfHalle.de</a>)
+            </p>
+            <h3 className={styles.bold}>Internetauftritt</h3>
+            <p>Anbieter und verantwortlich für den Inhalt dieser Seite ist</p>
+            <p>
+                Antonia Gerdemann
+                <br />
+                Wörmlitzer Str. 101
+                <br />
+                06110 Halle/S.
+            </p>
+            <p>
+                Entwickelt hat sie Benjamin Schandera aus Halle (husterknupp at outlook.com) mit{' '}
+                <a href={'https://github.com/Husterknupp/2020-oster-squash'} target={'_blank'}>
+                    freier Software
+                </a>
+                , gehostet auf GitHub.com und{' '}
+                <a href={'https://uberspace.de'} target={'_blank'}>
+                    Uberspace.de
+                </a>
+            </p>
+            <h3 className={styles.bold}>Das krasse Bild</h3>
+            <p>
+                Im Titel hat uns frei zur Verfügung gestellt: Bruno von der Kraan (
+                <a href={'https://unsplash.com/photos/v2HgNzRDfII'} target={'_blank'}>
+                    Unsplash.com
+                </a>
+                )
+            </p>
         </div>
     );
 }
