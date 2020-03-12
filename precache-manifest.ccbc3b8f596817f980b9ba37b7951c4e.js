8666909f084068a7f1f4bfe41f20b6ca2b1e95e5
bens <bens@spreadshirt.net>
2020-03-12 18:56:13 +0100

    Updates

--- precache-manifest.ccbc3b8f596817f980b9ba37b7951c4e.js
+++ precache-manifest.ccbc3b8f596817f980b9ba37b7951c4e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "886f8de3de32ba946eb21eff71d465a1",
+    "revision": "a4ea0a97ba7319e49bf5aedc9b80d8e8",
     "url": "/index.html"
   },
   {
-    "revision": "c929baea3fea6291aac8",
+    "revision": "861c87a3ac54f3b1e351",
     "url": "/static/css/main.0083f044.chunk.css"
   },
   {
-    "revision": "137991dea9c96bb8a6aa",
-    "url": "/static/js/2.ca7c10d1.chunk.js"
+    "revision": "cd7e25c9dc908705195f",
+    "url": "/static/js/2.aa8629e3.chunk.js"
   },
   {
     "revision": "78ddf5a465591a03b9601ba2ddd63415",
-    "url": "/static/js/2.ca7c10d1.chunk.js.LICENSE"
+    "url": "/static/js/2.aa8629e3.chunk.js.LICENSE"
   },
   {
-    "revision": "c929baea3fea6291aac8",
-    "url": "/static/js/main.2eac3e3d.chunk.js"
+    "revision": "861c87a3ac54f3b1e351",
+    "url": "/static/js/main.04bc177b.chunk.js"
   },
   {
     "revision": "a8139511e7e3c6f8467f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.545bbb5faa282f8881da527620d5c106.js"
+  "/precache-manifest.ccbc3b8f596817f980b9ba37b7951c4e.js"
 );
 
 self.addEventListener('message', (event) => {
